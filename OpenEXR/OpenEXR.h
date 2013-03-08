ef70a09055b54bd9be154d6a54480322c975f243
Javier Edo <byexavi@gmail.com>
2013-03-08 21:11:33 +0000

    Initial Commit

---
+++ OpenEXR/OpenEXR.h
@@ -0,0 +1,13 @@
+//
+//  OpenEXR.h
+//  OpenEXR
+//
+//  Created by Javier Edo Meseguer on 08/03/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OpenEXR : NSObject
+
+@end
---
+++ OpenEXR/OpenEXR.m
@@ -0,0 +1,13 @@
+//
+//  OpenEXR.m
+//  OpenEXR
+//
+//  Created by Javier Edo Meseguer on 08/03/13.
+//
+//
+
+#import "OpenEXR.h"
+
+@implementation OpenEXR
+
+@end
